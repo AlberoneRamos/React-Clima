8cceff92476cd99e806b978afdd8da8250d47480
DJDiney <rickeight@hotmail.com>
2017-05-15 17:59:22 -0300

    Alterações na página "sobre" e "exemplos"

--- app/components/Exemplos.jsx
+++ app/components/Exemplos.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default class Exemplos extends React.Component{
     render(){
         return(
-            <h3>Exemplos:</h3>
+            <div>
+                <h3>Exemplos:</h3>
+                <p>Bem-vindo à página de exemplos</p>
+            </div>
         );
     }
 }
--- app/components/Sobre.jsx
+++ app/components/Sobre.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default class Sobre extends React.Component{
     render(){
         return(
-        <h3>Sobre o app</h3>
+            <div>
+                <h3>Sobre o app</h3>
+                <p>Bem-vindo à página de informações!</p>
+            </div>
         );
     }
 }
