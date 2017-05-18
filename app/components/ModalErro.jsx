80d79d788195bbea947a3198f484de7ee44a1020
DJDiney <rickeight@hotmail.com>
2017-05-18 19:15:13 -0300

    Bugfix do modal de erro consertado.

--- app/components/ModalErro.jsx
+++ app/components/ModalErro.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
 
 export default class ModalErro extends React.Component{
     
@@ -9,13 +11,8 @@ export default class ModalErro extends React.Component{
 
 
     componentDidMount(){
-        var Modal = new Foundation.Reveal($('#modal-erro'));
-        Modal.open();
-    }
-
-    render(){
         var {titulo, mensagem} = this.props;
-        return(
+        var modalMarkup = (
             <div className="reveal tiny text-center" id="modal-erro" data-reveal="">
                 <h1>{titulo}</h1>
                 <p className="lead">{mensagem}</p>
@@ -23,6 +20,17 @@ export default class ModalErro extends React.Component{
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
+        );
+
+        var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+        $(ReactDOM.findDOMNode(this)).html($modal);
+        var Modal = new Foundation.Reveal($('#modal-erro'));
+        Modal.open();
+    }
+
+    render(){
+        return(
+            <div></div>
         );
     }
 }
